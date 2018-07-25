5289dafb1fb6272dbb40b887a43338d081e87be2
yustianbudiman <blog3489@gmail.com>
2018-07-25 14:06:46 +0700

    Create submit.js

---
+++ submit.js
@@ -0,0 +1,16 @@
+
+$(document).on('submit', 'form.AjaxForm', function() {            
+        $.ajax({
+            url     : $(this).attr('action'),
+            type    : $(this).attr('method'),
+            dataType: 'json',
+            data    : $(this).serialize(),
+            success : function( data ) {
+                         alert('Submitted');
+            },
+            error   : function( xhr, err ) {
+                         alert('Error');     
+            }
+        });    
+        return false;
+    });
